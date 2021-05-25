548db4130dd7e3ebb71bbb41028790f0998fc559
dekapaan <54145779+dekapaan@users.noreply.github.com>
2021-05-25 14:29:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from tkinter import messagebox
 
 class numToList:
     def __init__(self, master):
+        # changing window configurations
         self.master = master
         self.master.title('Adding numbers to a sorted list')
         self.master.config(bg='#575366')
         self.master.geometry('490x150')
 
+        # User entry label and entry field
         self.enter_lbl = Label(self.master, text='Enter number:', font='monospace 11', bg='#575366', fg='white')
         self.enter_entry = Entry(self.master, font='monospace 10', bg='#D1E3DD')
         self.enter_btn = Button(self.master, text='Add to list', font='monospace 10', height=1, command=self.add_sort,
@@ -17,11 +19,13 @@ class numToList:
         self.enter_entry.place(x=170, y=10)
         self.enter_btn.place(x=350, y=7)
 
+        # Frame and label to display list
         self.list_frame = Frame(self.master, bg='#222', width=445, height=30)
         self.list_lbl = Label(self.list_frame, bg='#222', fg='white')
         self.list_frame.place(x=20, y=70)
         self.list_lbl.place(x=1, y=1)
 
+        # Buttons
         self.show_list_btn = Button(self.master, text='Display list', command=self.show_list, bg='#D1E3DD')
         self.clear_btn = Button(self.master, text='Clear', command=self.clear, bg='#D1E3DD')
         self.exit_btn = Button(self.master, text='Exit', command=exit, bg='#D1E3DD')
@@ -31,10 +35,12 @@ class numToList:
 
         self.num_list = list()
 
+    # function to clear fields
     def clear(self):
         self.enter_entry.delete(0, END)
         self.list_lbl.config(text='')
 
+    # funtion to add new number to list and bubble-sort the list
     def add_sort(self):
         try:
             number = int(self.enter_entry.get())
@@ -47,9 +53,11 @@ class numToList:
                         if self.num_list[j] > self.num_list[j + 1]:
                             self.num_list[j], self.num_list[j + 1] = self.num_list[j + 1], self.num_list[j]
 
+        # ValueError if integer not entered
         except ValueError:
             messagebox.showerror(message='Only integer allowed as entry')
 
+    # Function to display list
     def show_list(self):
         self.list_lbl.config(text=self.num_list)
 
